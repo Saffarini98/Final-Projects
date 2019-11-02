81145379f098b0e0a2936d408357c55d2fafb3e3
Maxim <maximsaffarini@gmail.com>
2019-11-02 17:18:39 +0100

    Almost final version of my project

--- SmartDoe/server.js
+++ SmartDoe/server.js
@@ -1,24 +1,35 @@
 const path = require("path");
+const sassMiddleware = require("node-sass-middleware");
+
 var express = require('express');
+var exphbs  = require('express-handlebars');
 var app = express();
 
 
-// app.get('/', function (req, res) {
-//     res.render('home');
-// });
-
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
 app.get('/', function(req, res) {
-  res.sendfile('index.html');
+  // res.sendfile('index.html');
+  res.render('home');
 });
 
-app.use(express.static(path.join(__dirname, 'html')));
+app.get('/about', function(req, res) {
+  res.render('about');
+});
 
+// app.use(express.static(path.join(__dirname, 'public')));
 
-// app.get('/about', function (req, res) {
-//     res.render('about');
-// });
-
+app.use(
+  sassMiddleware({
+    /* Options */
+    src: path.join(__dirname, "scss"),
+    dest: path.join(__dirname, "public/css/"),
+    // debug: true,
+    outputStyle: "compressed",
+    prefix: "/css"
+  })
+);
 
 app.use(express.static("public"));
 app.listen(3000);
